17fb5094f728ed989bb404b319ed456eb760f803
dbtjswjd <163017875+dbtjswjd@users.noreply.github.com>
2025-11-14 03:22:24 +0900

    마무리

---
+++ test_page/main.js
@@ -0,0 +1,20 @@
+const delay = 9000;
+
+const debounce = (fn, delay) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
+};
+
+// const callbackFn = () => (window.location.href = 'https://www.naver.com');
+const callbackFn = () => {
+  window.location.href = 'index.html';
+};
+
+const debouncedFn = debounce(callbackFn, delay);
+
+['pointerdown', 'pointermove', 'keydown'].forEach((event) =>
+  window.addEventListener(event, debouncedFn)
+);
